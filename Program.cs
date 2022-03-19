1ba91966076e3818f4caf5c814ea8d6168b1c392
Liubov K <lyouso@yandex.ru>
2022-03-19 12:47:39 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+DayOfTheWeek(number);
+
+void DayOfTheWeek(int number1)
+{
+   while (number1 <= 7)
+   {
+       if (number1 < 6) Console.WriteLine(number1 + " - это будний день");
+       else Console.WriteLine(number1 + " - это выходной");
+       break;
+   }
+
+}
